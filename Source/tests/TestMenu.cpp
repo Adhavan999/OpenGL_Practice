67b0e0a6a187046149cafc1d2d69d158f40fab4e
Adhavan <adhavthiru@gmail.com>
2020-03-20 01:03:59 +0530

    Adding TestMenu Files and organized some code with other tests

---
+++ Source/tests/TestMenu.cpp
@@ -0,0 +1,20 @@
+#include "tests/TestMenu.h"
+
+namespace test
+{
+	TestMenu::TestMenu()
+	{
+	}
+	TestMenu::~TestMenu()
+	{
+	}
+	void TestMenu::OnUpdate(float deltaTime)
+	{
+	}
+	void TestMenu::OnRender()
+	{
+	}
+	void TestMenu::OnImGuiRender()
+	{
+	}
+}
--- Source/tests/TestMenu.h
+++ Source/tests/TestMenu.h
@@ -1,17 +1,14 @@
 #pragma once
-
 #include "Test.h"
-#include <imgui/imgui.h>
 
 namespace test
 {
-	class TestClearColor : public Test
+	class TestMenu : public Test
 	{
 	private:
-		float m_ClearColor[4];
 	public:
-		TestClearColor();
-		~TestClearColor() override;
+		TestMenu();
+		~TestMenu() override;
 
 		void OnUpdate(float deltaTime) override;
 		void OnRender() override;
